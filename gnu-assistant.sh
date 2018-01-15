299132bd155734d70694c1d0680ca614ff2e9d5f
Jon Reinhold <jon.reinhold15@gmail.com>
2018-01-15 17:58:52 -0600

    Update gnu-assistant.sh

--- gnu-assistant.sh
+++ gnu-assistant.sh
@@ -1,2 +1,5 @@
+#cortana eat your heart out 
+#ran over ssh on android upon connecting to home network
+
 spd-say -t female3 "Welcome Home, Jon. Here is your evening forecast"
 weather Kansas City | spd-say -et female3
